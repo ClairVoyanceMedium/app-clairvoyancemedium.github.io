97a4262b0fac5382ed077b3608a8022b8db482f8
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2026-09-11 16:05:03 +0200

    Ajoute la configuration publique du service push

---
+++ push-config.js
@@ -0,0 +1,7 @@
+window.CVM_PUSH_CONFIG = {
+  // L'App ID OneSignal n'est pas un secret. Il sera renseigné ici dès que
+  // l'application OneSignal ClairVoyanceMedium.com aura été créée.
+  oneSignalAppId: '',
+  serviceWorkerPath: 'OneSignalSDKWorker.js',
+  serviceWorkerScope: '/app-clairvoyancemedium.github.io/'
+};
