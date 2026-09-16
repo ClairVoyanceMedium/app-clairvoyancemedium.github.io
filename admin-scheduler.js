c54bcfefc9f6c7c78b9d31f2a7e829f1229047f4
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2026-09-16 18:08:21 +0200

    Load safe dashboard reset controls

--- admin-scheduler.js
+++ admin-scheduler.js
@@ -216,3 +216,10 @@
 
   resetHorizontalPosition();
 })();
+
+(()=>{
+  const script=document.createElement('script');
+  script.src='admin-resets.js?v=20260916-1';
+  script.defer=true;
+  document.body.appendChild(script);
+})();
