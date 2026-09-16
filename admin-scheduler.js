19d6018a6bb2dbceb72acaba5e0e459935634c41
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2026-09-16 17:28:50 +0200

    Corrige l’échappement du tableau de bord

--- admin-scheduler.js
+++ admin-scheduler.js
@@ -45,7 +45,7 @@
   const deliveryHelp=$('deliveryHelp');
   const scheduleTimezone=$('scheduleTimezone');
 
-  function esc(s){return String(s??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#39;'}[c]))}
+  function esc(s){return String(s??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]))}
   function setStatus(msg,type=''){status.className='status'+(type?' '+type:'');status.innerHTML=msg}
   function token(){return localStorage.getItem('cvm_admin_token')||sessionStorage.getItem('cvm_admin_token')||''}
   function headers(){return {'Authorization':'Bearer '+token(),'Accept':'application/vnd.github+json','X-GitHub-Api-Version':'2022-11-28'}}
